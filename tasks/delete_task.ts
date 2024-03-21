6ee5af72b0012fa1c7085aabe8e8dc2e4cc9530f
Octo8080 <okutannderuta0529@gmail.com>
2024-03-21 23:34:14 +0900

    update task

--- tasks/delete_task.ts
+++ tasks/delete_task.ts
@@ -1,3 +1,4 @@
+import { CONSTS } from "../utils/consts.ts";
 import { deleteByDate } from "../utils/kvstorage.ts";
 import { getBeforeDate } from "../utils/time.ts";
 
@@ -5,7 +6,7 @@ export async function deleteData() {
   console.log("Start Task: deleteData");
 
   const date = new Date();
-  const dateString = getBeforeDate(date, 1);
+  const dateString = getBeforeDate(date, Number(CONSTS.DELETE_DATE));
   console.log("delete date: ~", dateString);
 
   await deleteByDate(dateString);
--- utils/consts.ts
+++ utils/consts.ts
@@ -1,4 +1,5 @@
 export const CONSTS = {
   APP_ENV: Deno.env.get("APP_ENV") || "development",
   ALLOWED_ORIGIN: Deno.env.get("ALLOWED_ORIGIN")!,
+  DELETE_DATE: Deno.env.get("DELETE_DATE")!,
 };
