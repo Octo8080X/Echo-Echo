a46cc819e3004ad2c5fe3848322154efd7b87e3c
Octo8080 <okutannderuta0529@gmail.com>
2024-03-26 22:15:48 +0900

    fix: typo welcom => welcome

--- components/HeroIndex.tsx
+++ components/HeroIndex.tsx
@@ -6,8 +6,8 @@ export default function Hero() {
     >
       <div class="hero-overlay bg-opacity-40"></div>
       <div class="hero-content text-center text-neutral-content">
-        <div class="max-w-md">
-          <h1 class="text-5xl font-bold">Welcom `Echo-Echo`</h1>
+        <div class="">
+          <h1 class="text-5xl font-bold">Welcome `Echo-Echo`</h1>
           <div class="py-3">
             <p class="">
               `Echo-Echo` is a short sound registration service!
